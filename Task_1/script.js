d4ae4a4f97ef398388ec82bc408921b293d54108
Rina-Silver <katerina.unz@gmail.com>
2020-11-01 17:55:57 +0700

    Скрипт для всех произвольных эл-тов

--- Task_1/script.js
+++ Task_1/script.js
@@ -1,11 +1,10 @@
 (function () {
     let items = document.querySelectorAll('.card-md, .card-lg, .card-sm');
-    let item = null;
     let intervalId = setInterval(function() {
-        if (item != null){item.style.boxShadow = "";}
-        
-        item = items[Math.floor(Math.random() * items.length)];
+        let item_1 = items[Math.floor(Math.random() * items.length)];
         let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        item.style.boxShadow = `0 0 20px #${randomColor}`;
+        item_1.style.boxShadow = `0 0 20px #${randomColor}`;
+        let item_2 = items[Math.floor(Math.random() * items.length)];
+        item_2.style.boxShadow = "";
     }, 2000);
  }());
